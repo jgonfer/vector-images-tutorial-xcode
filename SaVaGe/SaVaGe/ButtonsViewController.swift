181a97265fa5c943ef491dad9bede6777f44ed12
jgonfer <josep.gonfer@gmail.com>
2017-10-30 23:42:52 +0100

    Empty project

---
+++ SaVaGe/SaVaGe/ButtonsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ButtonsViewController.swift
+//  SaVaGe
+//
+//  Created by Josep Gonzalez Fernandez on 30/10/17.
+//  Copyright © 2017 Dreams Corner. All rights reserved.
+//
+
+import UIKit
+
+class ButtonsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SaVaGe/SaVaGe/ImagesViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ImagesViewController.swift
+//  SaVaGe
+//
+//  Created by Josep Gonzalez Fernandez on 30/10/17.
+//  Copyright © 2017 Dreams Corner. All rights reserved.
+//
+
+import UIKit
+
+class ImagesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
